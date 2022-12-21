39c1ad2486b86d3949ce2f49a0516d9b869f193a
max <copita.maxim.info@gmail.com>
2022-12-21 16:30:46 +0200

    Create Spring boat project

---
+++ riot/src/test/java/com/src/riot/RiotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.riot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
