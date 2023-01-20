578ace8750a1de6a894bb88fdea6c7f11ba3a649
max <copita.maxim.info@gmail.com>
2023-01-20 11:35:21 +0200

    Add route to singUp and login buttons

--- riot_react/src/components/Navbar.jsx
+++ riot_react/src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import SearchBox from './Searchbox'
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-purple-600 text-3xl font-bold cursor-pointer'>RIOT STREAMING SERVICE</h1>
-        <div>
-          {/* <SearchBox></SearchBox> */}
+    return (
+        <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+            <Link to="/">
+                <h1 className='text-purple-600 text-3xl font-bold cursor-pointer'>RIOT STREAMING SERVICE</h1>
+            </Link>
+            <div>
+                {/* <SearchBox></SearchBox> */}
+            </div>
+            <div><Link to="/auth/login">
+                <button className='text-white font-bold pr-4'>Sign In</button>
+            </Link>
+                <Link to="/register">
+                    <button className='bg-purple-600 font-bold px-3 py-2 rounded cursor-pointer text-black'>Sign Up
+                    </button>
+                </Link>
+            </div>
         </div>
-        <div>
-            <button className='text-white font-bold pr-4'>Sign In</button>
-            <button className='bg-purple-600 font-bold px-3 py-2 rounded cursor-pointer text-black'>Sign Up</button>
-        </div>
-    </div>
-  )
+    )
 }
 export default Navbar
