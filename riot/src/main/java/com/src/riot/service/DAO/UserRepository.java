3719cffa75da127ad38c623a55540d14d0b3b374
max <copita.maxim.info@gmail.com>
2023-01-20 11:25:21 +0200

    Create query for user repository

--- riot/src/main/java/com/src/riot/service/DAO/UserRepository.java
+++ riot/src/main/java/com/src/riot/service/DAO/UserRepository.java
@@ -3,14 +3,35 @@ package com.src.riot.service.DAO;
 import com.src.riot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-//    User createUser(User user);
+import java.util.List;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long> {
+    //    User createUser(User user);
     @Query("SELECT u FROM User u where u.userEmail=?1")
     User userLoginEmail(String userEmail);
+
     @Query("SELECT u FROM User u where u.userPassword=?1 and u.userEmail=?2")
-    User userLoginPass(String userPassword,String userEmail);
+    User userLoginPass(String userPassword, String userEmail);
+
+    @Query("SELECT username from User where username=?1")
+    User findByUserName(String Name);
+
+    @Query("SELECT userEmail from User where userEmail=?1")
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u from User u where u.username=?1 ")
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    @Query("select id from User where id=?1")
+    List<User> findByIdIn(List<Long> userIds);
+
+    @Query("SELECT username from User where username=?1")
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+//    Boolean existsByEmail(String email);
 //    boolean checkUserDetails(User user);
 //
 //    void removeUser(User user);
