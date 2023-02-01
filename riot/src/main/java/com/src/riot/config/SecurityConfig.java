a019aab1cc6ec905873a5c1207e644dbafc40ce8
MaxOverPowered <copita.maxim.info@gmail.com>
2023-02-01 12:53:43 +0200

    Remove all comments and refactoring code

--- riot/src/main/java/com/src/riot/config/SecurityConfig.java
+++ riot/src/main/java/com/src/riot/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
@@ -90,6 +91,7 @@ public class SecurityConfig {
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout")
+                .deleteCookies("JSESSIONID")
                 .permitAll();
 
 
