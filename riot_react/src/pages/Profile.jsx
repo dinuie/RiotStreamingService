c6cb6f8177286ea9d29a51e7393639ab712dfcb1
cristimihaescu <cristimihaescu24@yahoo.ro>
2023-02-13 13:30:45 +0200

    user page

--- riot_react/src/pages/Profile.jsx
+++ riot_react/src/pages/Profile.jsx
@@ -1,14 +1,19 @@
 import { color } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProfilePage = () => {
   return (
-    <div style ={{backgroundColor: "#111827" , color:"white"}}>
-      <h1>My Profile</h1>
-      <p>Name: John Doe</p>
-      <p>Occupation: Software Engineer</p>
-      <p>Location: San Francisco, CA</p>
+    <div style ={{backgroundColor: "#111827" , color:"white", textAlign: "center"}}>
+      <h1>Hello User !</h1>
+      <p>Username: John Doe</p>
+      <p>Date of Birth: 20-09-1995</p>
+      <p>email: admin@admin.com</p>
       <p>About Me: </p>
+      <p>Change password:</p>
+      <Link to="/">
+      <button className='text-black font-sans bg-gradient-to-r from-purple-600 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-semibold rounded-lg text-sm px-5 py-2.5 text-center'>Back to Homepage</button>
+      </Link>
     </div>
   );
 };
