c3e85b8d02cc73beda3eb089af2f5b59a83b06cd
cristimihaescu <cristimihaescu24@yahoo.ro>
2023-02-15 14:46:33 +0200

    working like button

--- riot_react/src/App.js
+++ riot_react/src/App.js
@@ -10,7 +10,6 @@ import Welcome from "./pages/Welcome";
 import ProfilePage from "./pages/Profile";
 
 function App() {
-  // const navigate = useNavigate(); // React Hook
   const isLoggedIn = useCurrentUser();
 
   return (
