7994f42449558048dae265f5695d0f8a74f8315a
MaxOverPowered <copita.maxim.info@gmail.com>
2023-02-27 14:42:55 +0200

    initialize database add movie user role and movie genre when program is running

---
+++ riot/src/main/java/com/src/riot/config/InitializeData.java
@@ -0,0 +1,31 @@
+package com.src.riot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class InitializeData {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadData() {
+        Resource resource1 = new ClassPathResource("roles.sql");
+        Resource resource2 = new ClassPathResource("moviegenre.sql");
+        Resource resource3 = new ClassPathResource("movies.sql");
+        ResourceDatabasePopulator populator1 = new ResourceDatabasePopulator(false, false, "UTF-8", resource1);
+        ResourceDatabasePopulator populator2 = new ResourceDatabasePopulator(false, false, "UTF-8", resource2);
+        ResourceDatabasePopulator populator3 = new ResourceDatabasePopulator(false, false, "UTF-8", resource3);
+        populator1.execute(dataSource);
+        populator2.execute(dataSource);
+        populator3.execute(dataSource);
+    }
+}
