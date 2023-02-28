78566bcb766f40d624f9c4a9ddea8210f5dbcc82
MaxOverPowered <copita.maxim.info@gmail.com>
2023-02-28 13:28:31 +0200

    initialize movies json

--- riot/src/main/java/com/src/riot/service/MovieService.java
+++ riot/src/main/java/com/src/riot/service/MovieService.java
@@ -1,9 +1,13 @@
 package com.src.riot.service;
-
+;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import com.src.riot.model.Movie;
 import com.src.riot.service.DAO.*;
 import org.springframework.stereotype.Service;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -38,6 +42,46 @@ public class MovieService extends BaseService {
 
     public Optional<Movie> findById(Long id) {
         return movieRepository.findById(id);
+    }
+    public void jsonMovieInDatabase() {
+        String filename = "src/main/java/com/src/riot/config/jsonMovie.json";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+            JSONArray jsonArray = new JSONArray(sb.toString());
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObj = jsonArray.getJSONObject(i);
+                Movie movie = new Movie();
+                movie.setId(jsonObj.getLong("id"));
+                movie.setImdb_id(jsonObj.getString("imdb_id"));
+                movie.setTitle(jsonObj.getString("title"));
+                movie.setEnglish_title(jsonObj.getString("english_title"));
+                movie.setBackdrop_path(jsonObj.getString("backdrop_path"));
+                movie.setImdb(jsonObj.getFloat("imdb"));
+                movie.setRelease_date(jsonObj.getString("release_date"));
+                movie.setRuntime(jsonObj.getInt("runtime"));
+                JSONArray genreIdsArray = jsonObj.getJSONArray("genre_ids");
+                List<Integer> genreIdsList = new ArrayList<>();
+                for (int j = 0; j < genreIdsArray.length(); j++) {
+                    int genreId = genreIdsArray.getInt(j);
+                    genreIdsList.add(genreId);
+                }
+                movie.setGenre_ids(genreIdsList);
+                movie.setHash(jsonObj.getString("hash"));
+                movie.setUpdate(jsonObj.getString("updated"));
+                movieRepository.save(movie);
+            }
+            System.out.println("JSON data inserted successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Set<String> findGenresByMovieId(Long movieId) {
